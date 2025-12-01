d9e66966d85f3ae440e8169fdfe08b6fb7835139
AndreyRuRO28 <151657314+AndreyRuRO28@users.noreply.github.com>
2025-12-01 02:37:57 -0600

    CRUD de usuarios totalmente funcional. Ya se puede crear, obtener, actualizar y eliminar usuarios sin problemas. Agregué validaciones para evitar datos incompletos y manejo de errores más claro para que el flujo sea fácil de entender. En el update implementé una lógica que mantiene los datos anteriores si no se envían en el body, así no se borran valores por accidente.

---
+++ Backend/src/app.js
@@ -0,0 +1,46 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import { config } from './config/config.js';
+import ejerciciosRoutes from './routes/ejerciciosRoutes.js';
+import rutinasRoutes from './routes/rutinasRoutes.js';
+import usuariosRoutes from './routes/usuarioRoute.js';
+import pool from './services/db.js';
+
+const app = express();
+
+app.use(cors({ origin: config.CORS_ORIGIN }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/ejercicios', ejerciciosRoutes);
+app.use('/api/rutinas', rutinasRoutes);
+app.use('/api/usuarios', usuariosRoutes);
+
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'OK', message: 'API Rutinas Fitness funcionando correctamente' });
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Error interno del servidor' });
+});
+
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Ruta no encontrada' });
+});
+
+(async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log(' Conectado a MariaDB en Azure, sos demasiado bueno');
+    connection.release();
+  } catch (err) {
+    console.error(' Error conectando a la DB, que idiota que sos:', err);
+  }
+})();
+
+export default app;
